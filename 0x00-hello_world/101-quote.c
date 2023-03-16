b7846226c061f0af1cf691ab99861105ebaafb75
Mahmoud Salah <ms685116@gmial.com>
2023-03-16 12:47:25 -0700

    quoting

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ * Return: return 1 if true
+ */
+int main(void)
+{
+	char *msg = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+
+	write(2, msg, sizeof(msg));
+	return (0);
+}
