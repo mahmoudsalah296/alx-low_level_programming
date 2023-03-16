3cab62b353c529fca3de4a85640085ac341ffdef
Mahmoud Salah <ms685116@gmial.com>
2023-03-16 08:49:57 -0700

    resolve 6

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,4 +1,8 @@
 #include<stdio.h>
+/**
+ * main - the main function
+ * @x: the int var
+ */
 int main(void)
 {
 	int x = 10;
@@ -6,10 +10,11 @@ int main(void)
 	char c = 'm';
 	long long int llx = 100;
 	float f = 10.0f;
-	printf("Size of a char: %lu byte(s)\n",sizeof(c));
-	printf("Size of an int : %lu byte(s)\n",sizeof(x));
-	printf("Size of a long int : %lu byte(s)\n",sizeof(lx));
-	printf("Size of a long long int : %lu byte(s)\n",sizeof(llx));
-	printf("Size of a float : %lu byte(s)\n",sizeof(f));
+
+	printf("Size of a char: %lu byte(s)\n", sizeof(c));
+	printf("Size of an int: %lu byte(s)\n", sizeof(x));
+	printf("Size of a long int: %lu byte(s)\n", sizeof(lx));
+	printf("Size of a long long int: %lu byte(s)\n", sizeof(llx));
+	printf("Size of a float: %lu byte(s)\n", sizeof(f));
 	return (0);
 }
