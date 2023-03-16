2d166eba28647b69e35195d4f7d1b1df8317d6a1
Mahmoud Salah <ms685116@gmial.com>
2023-03-16 12:52:33 -0700

    quoting

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,12 +1,13 @@
 #include <stdio.h>
+#include <unistd.h>
 /**
- * main - entry point
- * Return: return 1 if true
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
-	char *msg = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
-
-	write(2, msg, sizeof(msg));
-	return (0);
+write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",
+sizeof("and that piece of art is useful\" - Dora Korpar, 2015-10-19"));
+return (1);
 }
