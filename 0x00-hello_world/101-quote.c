5b42ee84dfbb1b85b621ea9cb368f5bd690e5ad7
Mahmoud Salah <ms685116@gmial.com>
2023-03-16 10:51:53 -0700

    write to stderr

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,11 +1,12 @@
 #include <stdio.h>
+#include <string.h>
 /**
  * main - this is main
  * Return: return 1 if true
  */
 int main(void)
 {
-	const char *str = "and that piece of art is useful\" - Dora Korpar, 2015-10-19";
-	write(2, str, strlen(str));
+	const char *msg = "and that piece of art is useful\" - Dora Korpar, 2015-10-19";
+	write(2, msg, strlen(msg));
 	return (1);
 }
