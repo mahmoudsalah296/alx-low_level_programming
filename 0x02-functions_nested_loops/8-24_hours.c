29d50550602cbcd9c78fe29caecb455c11639d6d
Mahmoud Salah <ms685116@gmial.com>
2023-03-21 13:10:22 -0700

    24 hours

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -6,58 +6,20 @@ void jack_bauer(void)
 {
 	int i = 0;
 	int j = 0;
-	int k = 0;
-	int m = 0;
 
-	while (i <= 2)
+	while (i <= 23)
 	{
-		if (i < 2)
+		j = 0;
+
+		while (j <= 59)
 		{
-			j = 0;
-			while (j <= 9)
-			{
-				k = 0;
-				while (k <= 5)
-				{
-					m = 0;
-					while (m <= 9)
-					{
-						putchar(i + '0');
-						putchar(j + '0');
-						putchar(':');
-						putchar(k + '0');
-						putchar(m + '0');
-						putchar('\n');
-						m++;
-					}
-					k++;
-				}
-				j++;
-			}
-		}
-		else if (i == 2)
-		{
-			j = 0;
-			while (j <= 3)
-			{
-				k = 0;
-				while (k <= 5)
-				{
-					m = 0;
-					while (m <= 9)
-					{
-						putchar(i + '0');
-						putchar(j + '0');
-						putchar(':');
-						putchar(k + '0');
-						putchar(m + '0');
-						putchar('\n');
-						m++;
-					}
-					k++;
-				}
-				j++;
-			}
+			_putchar(i / 10 + '0');
+			_putchar(i % 10 + '0');
+			_putchar(':');
+			_putchar(j / 10 + '0');
+			_putchar(j % 10 + '0');
+			_putchar('\n');
+			j++;
 		}
 		i++;
 	}
