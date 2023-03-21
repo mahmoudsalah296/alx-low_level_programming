d029ff9c01f2042016fdcb9c60901c2aae9590d5
Mahmoud Salah <ms685116@gmial.com>
2023-03-21 08:41:10 -0700

    check sign

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -8,17 +8,14 @@ int print_sign(int n)
 {
 	if (n > 0)
 	{
-		return (1);
-		putchar('+');
+		return (putchar('+'), 1);
 	}
 	else if (n < 0)
 	{
-		return (-1);
-		putchar('-');
+		return (putchar('-'), -1);
 	}
 	else
 	{
-		return (0);
-		putchar('0');
+		return (putchar('0'), 0);
 	}
 }
