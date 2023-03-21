cb143b0d2197e2f44d0b3f66917537df0818323e
Mahmoud Salah <ms685116@gmial.com>
2023-03-21 05:16:14 -0700

    print alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,14 @@
+/**
+  * print_alphabet - print alphabet ascendingly
+  */
+void print_alphabet(void)
+{
+	char c = 'a';
+
+	while (a <= 'z')
+	{
+		putchar(c);
+		c++;
+	}
+	putchar('\n');
+}
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,2 +1,2 @@
 #include "main.c"
-
+#include "1-alphabet.c"
