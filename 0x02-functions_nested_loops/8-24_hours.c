3f6a307f1a7b280408d0db96ba6a97b4b1779b47
Mahmoud Salah <ms685116@gmial.com>
2023-03-21 11:41:20 -0700

    24 hours

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+/**
+  * jack_bauer - print hour by minutes
+  */
+void jack_bauer(void)
+{
+	int i = 0;
+	int j = 0;
+	int k = 0;
+	int m = 0;
+
+	while (i <= 2)
+	{
+		_putchar(i + '0');
+		while (j <= 3)
+		{
+			_putchar(j + '0');
+			_putchar(':');
+			while (k <= 5)
+			{
+				_putchar(k + '0');
+				while (m <=9)
+				{
+					_putchar(m + '0');
+					_putchar('\n');
+					m++
+				}
+				k++
+			}
+			j++
+		}
+		i++
+	}
+}
