488bb11b8ed2d40480aad8657d62c03dd8640b4b
Mahmoud Salah <ms685116@gmial.com>
2023-03-22 04:16:01 -0700

    initial commit

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,13 +1,14 @@
-#include "main.h"
 #include <stdio.h>
+#include "main.h"
 /**
   * main - entry point
   * Return: always 0
   */
 int main(void)
 {
-	int i = 0;
+	int i;
 
+	i = 0;
 	positive_or_negative(i);
 	return (0);
 }
