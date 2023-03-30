4b5bebb64df7d2861bfc624ad9b0d57fdb833585
Mahmoud Salah <ms685116@gmial.com>
2023-03-30 13:51:30 -0700

    string compare

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -8,5 +8,14 @@
 */
 int _strcmp(char *s1, char *s2)
 {
-	return (s1[0] - s2[0]);
+	int i = 0;
+
+	while (s1[i] == s2[i])
+	{
+		if (s1[i] == '\0')
+			return (0);
+		i++;
+	}
+	return (s1[i] - s2[i]);
 }
+
