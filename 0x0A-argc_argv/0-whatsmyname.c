89b3b9003ce8ce60bc6052750c79172869451335
Mahmoud Salah <ms685116@gmial.com>
2023-04-14 10:11:22 -0700

    define unused variable

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -6,7 +6,8 @@
   * @argv: poiter to args
   * Return: 0 if success
   */
-int main(int argc, char *argv[])
+int main(__attribute__ ((unused))int argc, char *argv[])
 {
 	printf("%s\n", argv[0]);
+	return (0);
 }
