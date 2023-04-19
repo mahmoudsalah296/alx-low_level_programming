22d35e3369c19f8b76ac4136aae88b06ebc3e3ad
Mahmoud Salah <ms685116@gmail.com>
2023-04-20 00:08:21 +0200

    check if name is null

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,9 +1,13 @@
+#include <stdio.h>
 /**
  * print_name - prints a name
  * @name: name to be printed
  * @f: pointer to function that prints the name
- */
+*/
 void print_name(char *name, void (*f)(char *))
 {
-	f(name);
+	if (name != NULL && f != NULL)
+	{
+		f(name);
+	}
 }
