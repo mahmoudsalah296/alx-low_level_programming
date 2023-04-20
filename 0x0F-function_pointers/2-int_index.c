318ec98e78e6e4e2169af97c9f988221e64a563d
Mahmoud Salah <ms685116@gmail.com>
2023-04-20 13:54:28 +0200

    add checks if array or pointer are NULL

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -1,3 +1,4 @@
+#include <stdio.h>
 /**
  * int_index -  searches for an integer
  * @array: array of integers to be searched
@@ -10,12 +11,13 @@ int int_index(int *array, int size, int (*cmp)(int))
 {
 	int i;
 
-	if (size <= 0)
-		return (-1);
-	for (i = 0; i < size; i++)
+	if (array != NULL && size > 0 && cmp != NULL)
 	{
-		if (cmp(array[i]) == 1)
-			return (i);
+		for (i = 0; i < size; i++)
+		{
+			if (cmp(array[i]) == 1)
+				return (i);
+		}
 	}
 	return (-1);
 }
