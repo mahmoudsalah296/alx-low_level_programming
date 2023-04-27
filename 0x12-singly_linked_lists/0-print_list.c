d1f0ed66a6b4c583f94bb22a56a871e9ca13a112
Mahmoud Salah <ms685116@gmail.com>
2023-04-27 19:51:45 +0200

    initial commit

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,29 @@
+#include<stdio.h>
+#include"lists.h"
+/**
+ * print_list - prints all elements of a list
+ * @h: list to be printed
+ * Return: number of elements printed
+*/
+size_t print_list(const list_t *h)
+{
+	list_t *tmp;
+	size_t count = 0;
+
+	tmp = (list_t *)h;
+	if (h == NULL)
+		return (1);
+
+	while (tmp != NULL)
+	{
+		if (tmp->str == NULL)
+			printf("[0] (nil)\n");
+		else
+		{
+			printf("[%u] %s\n", tmp->len, tmp->str);
+		}
+		count++;
+		tmp = tmp->next;
+	}
+	return (count);
+}
