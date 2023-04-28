4db66b572cc91d15a70725199e4e59b2cbae9825
Mahmoud Salah <ms685116@gmail.com>
2023-04-28 21:58:02 +0200

    free a list2

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include"lists.h"
+
+/**
+ * free_listint2 - frees a list
+ * @head: head of the list
+*/
+void free_listint2(listint_t **head)
+{
+	listint_t *tmp = *head;
+
+	while (*head != NULL)
+	{
+		*head = (*head)->next;
+		free(tmp);
+		tmp = *head;
+	}
+	*head = NULL;
+}
