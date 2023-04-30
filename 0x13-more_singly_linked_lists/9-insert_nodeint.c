b385b3901573d6bfa4e04bb47a3d0df29ce6b07c
Mahmoud Salah <ms685116@gmail.com>
2023-04-30 20:56:19 +0200

    another update

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -1,52 +1,43 @@
 #include<stdlib.h>
 #include<stdio.h>
-#include"lists.h"
-
+#include "lists.h"
 /**
- * insert_nodeint_at_index - inserts a new node at a given position
- * @head: pointer to head of the list
- * @idx: index where the node will be inserted
- * @n: the data that will be inserted
- * Return: the address of the new node, or NULL if it failed
- */
+ * insert_nodeint_at_index - Inserts a new node at a given position
+ *
+ * @head: Pointer to pointer to the head of a list
+ * @idx: Index of the list where the new node should be added
+ * @n: Data to be added to the new node
+ *
+ * Return: Address of the new node, or NULL if it failed
+ **/
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
-{
-	listint_t *new_node = (listint_t *)malloc(sizeof(listint_t));
-	listint_t *tmp;
-	unsigned int i = 0;
+	{
+	listint_t *new_node, *prev_node;
+	unsigned int i;
 
+	if (head == NULL)
+		return (NULL);
+	new_node = malloc(sizeof(listint_t));
 	if (new_node == NULL)
-	{
-		free(new_node);
 		return (NULL);
-	}
-	if (*head == NULL)
+	new_node->n = n;
+	if (idx == 0)
 	{
-		return (NULL);
-	}
-	else
-	{
-		if (idx == 0)
-		{
-			new_node->n = n;
-			new_node->next = *head;
-			*head = new_node;
-		}
-		else
-		{
-			tmp = *head;
-			while (i < idx - 1)
-			{
-				tmp = tmp->next;
-				if (tmp == NULL)
-					return (NULL);
-				idx--;
-			}
-			new_node->n = n;
-			new_node->next = tmp->next;
-			tmp->next = new_node;
-		}
+		new_node->next = *head;
+		*head = new_node;
 		return (new_node);
 	}
+	prev_node = *head;
+	for (i = 0; prev_node != NULL && i < idx - 1; i++)
+	{
+		prev_node = prev_node->next;
+	}
+	if (prev_node == NULL || i != idx - 1)
+	{
+	free(new_node);
 	return (NULL);
+	}
+	new_node->next = prev_node->next;
+	prev_node->next = new_node;
+	return (new_node);
 }
