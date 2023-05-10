e796ebb3c212f0d6ba9230c4b1045f71e5321fc7
Mahmoud Salah <ms685116@gmail.com>
2023-05-10 23:24:30 +0200

    betty style

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -18,7 +18,9 @@ int create_file(const char *filename, char *text_content)
 	if (fd == -1)
 		return (-1);
 	if (text_content == NULL)
+	{
 		return (1);
+	}
 	else
 	{
 		write(fd, text_content, strlen(text_content));
