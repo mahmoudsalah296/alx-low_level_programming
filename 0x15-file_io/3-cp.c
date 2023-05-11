9446f3fb1bcfd32e20b672cf5a1af89025f4affd
Mahmoud Salah <ms685116@gmail.com>
2023-05-11 14:26:54 +0200

    close files if they can't be read or written

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -3,12 +3,14 @@
 #include <unistd.h>
 #include <stdlib.h>
 #define BUFF 1024
+
+void close_flie(fd);
 /**
  * main - entry point
  * @argc: number of arguments passed to main
  * @argv: array of arguments
  * Return: 0 on success
-*/
+ */
 int main(int argc, char *argv[])
 {
 	int fd1, fd2, nbytes, c;
@@ -24,6 +26,7 @@ int main(int argc, char *argv[])
 	if (fd1 == -1)
 	{
 		dprintf(2, "Error: Can't read from file %s\n", argv[1]);
+		close_flie(fd1);
 		exit(98);
 	}
 	/*open file two to copy into*/
@@ -31,23 +34,30 @@ int main(int argc, char *argv[])
 	if (fd2 == -1)
 	{
 		dprintf(2, "Error: Can't write to %s\n", argv[2]);
-		exit(98);
+		close_flie(fd2);
+		exit(99);
 	}
 	while ((nbytes = read(fd1, buffer, BUFF)) > 0)
 	{
 		write(fd2, buffer, nbytes);
 	}
-	c = close(fd1);
-	if (c < 0)
-	{
-		dprintf(2, "Error: Can't close fd %d\n", fd1);
-		exit(100);
-	}
-	c = close(fd2);
-	if (c < 0)
-	{
-		dprintf(2, "Error: Can't close fd %d\n", fd2);
-		exit(100);
-	}
+	close_flie(fd1);
+	close_flie(fd2);
 	return (0);
+}
+
+/**
+ * close_flie - closes a file and check if it is closed successfully
+ * @fd: file descriptor of file to be closed
+*/
+void close_flie(fd)
+{
+	int c;
+
+	c = close(fd);
+	if (c < 0)
+	{
+		dprintf(2, "Error: Can't close fd %d\n", fd);
+		exit(100);
+	}
 }
