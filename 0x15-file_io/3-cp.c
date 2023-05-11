b3dc2eba43ddb0709d52e6c940dabd39ac17c9ce
Mahmoud Salah <ms685116@gmail.com>
2023-05-11 13:35:52 +0200

    copy file to another

---
+++ 0x15-file_io/3-cp.c
@@ -0,0 +1,53 @@
+#include <stdio.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <stdlib.h>
+#define BUFF 1024
+/**
+ * main - entry point
+ * @argc: number of arguments passed to main
+ * @argv: array of arguments
+ * Return: 0 on success
+*/
+int main(int argc, char *argv[])
+{
+	int fd1, fd2, nbytes, c;
+	char buffer[BUFF];
+
+	if (argc != 3)
+	{
+		dprintf(2, "Usage: cp file_from file_to\n");
+		exit(97);
+	}
+	/*open file 1 to copy from*/
+	fd1 = open(argv[1], O_RDONLY);
+	if (fd1 == -1)
+	{
+		dprintf(2, "Error: Can't read from file %s\n", argv[1]);
+		exit(98);
+	}
+	/*open file two to copy into*/
+	fd2 = open(argv[2], O_TRUNC | O_RDWR | O_CREAT, 0664);
+	if (fd2 == -1)
+	{
+		dprintf(2, "Error: Can't write to %s\n", argv[2]);
+		exit(98);
+	}
+	while ((nbytes = read(fd1, buffer, BUFF)) > 0)
+	{
+		write(fd2, buffer, nbytes);
+	}
+	c = close(fd1);
+	if (c < 0)
+	{
+		dprintf(2, "Error: Can't close fd %d", fd1);
+		exit(100);
+	}
+	c = close(fd2);
+	if (c < 0)
+	{
+		dprintf(2, "Error: Can't close fd %d", fd2);
+		exit(100);
+	}
+	return (0);
+}
