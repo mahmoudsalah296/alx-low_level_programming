d4986c5a93cbbd5586a6c980b7cdc8e5720376aa
Mahmoud Salah <ms685116@gmail.com>
2023-07-06 22:58:32 +0200

    get bit of given index

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -7,12 +7,14 @@
 int get_bit(unsigned long int n, unsigned int index)
 {
 	unsigned int count = sizeof(n) * 8;
-
+	int bitValue;
+	unsigned long int bitMask;
+	
 	if (index >= count)
 		return (-1);
 
-	unsigned long int bitMask = 1 << index;
-	int bitValue = (n & bitMask) ? 1 : 0;
+	bitMask = 1 << index;
+	bitValue = (n & bitMask) ? 1 : 0;
 
 	return (bitValue);
 }
