c0a01c6affbd3b3685802450040f05cad929f64b
Mahmoud Salah <ms685116@gmail.com>
2023-09-08 20:56:08 +0300

    print hash tsble

--- 0x1A-hash_tables/5-hash_table_print.c
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -8,23 +8,23 @@
 */
 void hash_table_print(const hash_table_t *ht)
 {
-	hash_node_t *tmp = NULL;
 	unsigned long int i;
+	hash_node_t *tmp;
+	int elements_number = 0;
 
 	if (ht == NULL)
 		return;
+
 	printf("{");
 	for (i = 0; i < ht->size; i++)
 	{
-		if (ht->array[i] == NULL)
-			continue;
-
 		tmp = ht->array[i];
 		while (tmp)
 		{
-			printf("\'%s\':\'%s\'", ht->array[i]->key, ht->array[i]->value);
-			if (tmp->next)
+			if (elements_number > 0)
 				printf(", ");
+			printf("'%s': '%s'", tmp->key, tmp->value);
+			elements_number += 1;
 			tmp = tmp->next;
 		}
 	}
