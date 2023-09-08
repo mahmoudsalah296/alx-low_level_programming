f20433af4e6eec3ce106046c1f4f56a8088b5f4f
Mahmoud Salah <ms685116@gmail.com>
2023-09-08 15:29:36 +0300

    retrieve an element from hash table

--- 0x1A-hash_tables/4-hash_table_get.c
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -12,22 +12,22 @@
 */
 char *hash_table_get(const hash_table_t *ht, const char *key)
 {
-    unsigned long int index;
-    hash_node_t *tmp;
+	unsigned long int index;
+	hash_node_t *tmp;
 
-    if (ht == NULL || key == NULL)
-        return (NULL);
-    index = key_index((const unsigned char *)key, ht->size);
-    if (ht->array[index] == NULL)
-        return (NULL);
+	if (ht == NULL || key == NULL)
+		return (NULL);
+	index = key_index((const unsigned char *)key, ht->size);
+	if (ht->array[index] == NULL)
+		return (NULL);
 
-    tmp = ht->array[index];
-    while (tmp)
-    {
-        if (strcmp(tmp->key, key) == 0)
-            return tmp->value;
+	tmp = ht->array[index];
+	while (tmp)
+	{
+		if (strcmp(tmp->key, key) == 0)
+			return (tmp->value);
 
-        tmp =tmp->next;
-    }
-    return (NULL);
+		tmp = tmp->next;
+	}
+	return (NULL);
 }
