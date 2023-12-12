1207529f1174f9a1d0ec4cd30fe0659db2b5ead5
Mahmoud Salah <ms685116@gmail.com>
2023-12-12 19:48:48 +0200

    pass betty check

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -1,5 +1,12 @@
 #include "search_algos.h"
 
+/**
+ * linear_search - search in array using linear search
+ * @array: a pointer to the first element of the array to search in
+ * @size: the number of elements in array
+ * @value: the value to search for
+ * Return: the first index where value is located or -1 if value doesn't found
+ */
 int linear_search(int *array, size_t size, int value)
 {
 	size_t i = 0;
